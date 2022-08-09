7468b2178e80dd01628926b7dc0f6a4f6ade4bbe
Yura <steshenko.syv@gmail.com>
2022-08-09 14:39:49 +0300

    second edition

--- app/src/main/java/com/example/anagram/MainActivity.java
+++ app/src/main/java/com/example/anagram/MainActivity.java
@@ -31,26 +31,38 @@ public class MainActivity extends AppCompatActivity {
                 editString();
             }
 
-
             public void editString() {
                 String string = editText.getText().toString();
                 String[] strArray = string.split(" ");
                 StringBuilder builder = new StringBuilder();
 
-                for (int i = 0; i < strArray.length; i++) {
 
-                    char[] str1 = strArray[i].toCharArray();
-                    for (int j = str1.length - 1; j >= 0; j--) {
-                        if (str1[j] == 1) {
-                            break;
-                        } else {
-                            builder.append(str1[j]);
+                for (int n = 0; n < strArray.length; n++) {
 
+                    char[] str1 = strArray[n].toCharArray();
+
+
+                    char[] str2 = new char[str1.length];
+
+                    for (int i = 0; i < str1.length; i++) {
+
+                        for (int j = str1.length - i - 1; j >= 0; j--) {
+                            if (str1[i] == '0' || str1[i] == '1' || str1[i] == '2' || str1[i] == '3' || str1[i] == '4' || str1[i] == '5' || str1[i] == '6' || str1[i] == '7' || str1[i] == '8' || str1[i] == '9') {
+
+                                str2[i] = str1[i];
+
+                            } else {
+
+                                str2[j] = str1[i];
+                            }
+                            i++;
                         }
+                        builder.append(str2);
+                        builder.append(" ");
+
                     }
-                    builder.append(" ");
+                    outText.setText(builder.toString());
                 }
-                outText.setText(builder.toString());
             }
         };
         button.setOnClickListener(oclButton);
