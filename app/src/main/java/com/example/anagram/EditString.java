b9f1f1596fd40efd129db63d25d17ebf27fe3943
Yura <steshenko.syv@gmail.com>
2022-09-02 20:24:00 +0300

    1 working version, class  Edit string in separate file

---
+++ app/src/main/java/com/example/anagram/EditString.java
@@ -0,0 +1,54 @@
+package com.example.anagram;
+
+public class EditString {
+
+    public String editString(String string, String ignore) {
+
+            StringBuilder builder = new StringBuilder();
+            String[] strArray = string.split(" ");
+
+            for (int n = 0; n < strArray.length; n++) {
+                char[] s = strArray[n].toCharArray();
+                String revStr = new String(reverse(s, ignore));
+                builder.append(revStr);
+                builder.append(" ");
+            }
+            String result = builder.toString();
+            return result;
+        }
+
+        public char[] reverse(char[] s, String ignore) {
+
+            char[] temp = new char[s.length];           // creating character array
+            int x = 0;
+            for (int i = 0; i < s.length; i++) {
+                if (ignore.indexOf(s[i]) < 0) {
+                    temp[x] = s[i];
+                    x++;
+                }
+            }
+
+            rev(temp, 0, x);                          // reversing the character array
+
+            char[] anagram = new char[s.length];
+            x = 0;
+            for (int i = 0; i < s.length; i++) {        // creating reverse word
+                if (ignore.indexOf(s[i]) < 0) {
+                    anagram[i] = temp[x];
+                    x++;
+                } else {
+                    anagram[i] = s[i];
+                }
+            }
+            return anagram;
+        }
+
+        public void rev(char s[], int l, int r) {        // reversal array
+            for (int i = l; i < r / 2; i++) {
+                char temp = s[i];
+                s[i] = s[r - 1 - i];
+                s[r - 1 - i] = temp;
+            }
+        }
+    }
+
--- app/src/main/java/com/example/anagram/MainActivity.java
+++ app/src/main/java/com/example/anagram/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.anagram;
 
-import static android.view.View.*;
-
-import androidx.appcompat.app.AppCompatActivity;
+import static android.view.View.OnClickListener;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,61 +29,12 @@ public class MainActivity extends AppCompatActivity {
         OnClickListener oclButton = new OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditString edstr = new EditString();
-                edstr.editString();
-            }
-
-            class EditString {
-
-                public void editString() {
                 String string = editText.getText().toString();
-                String[] strArray = string.split(" ");
+                String ignore = ignoredCharacters.getText().toString();
 
-                StringBuilder builder = new StringBuilder();
+                EditString edstr = new EditString();
 
-                for (int n = 0; n<strArray.length; n++) {
-                    char[] s = strArray[n].toCharArray();
-
-                    String revStr = new String(reverse(s));
-                    builder.append(revStr);
-                    builder.append(" ");
-                    outText.setText(builder.toString());
-                }
-            }
-
-                public char[] reverse(char[] s) {
-                    String ignore = ignoredCharacters.getText().toString();
-                    char[] temp = new char[s.length];           // creating character array
-                    int x = 0;
-                    for (int i = 0; i < s.length; i++) {
-                        if (ignore.indexOf(s[i]) < 0) {
-                            temp[x] = s[i];
-                            x++;
-                        }
-                    }
-
-                    rev(temp, 0, x);                          // reversing the character array
-
-                    char[] anagram = new char[s.length];
-                    x = 0;
-                    for (int i = 0; i < s.length; i++) {        // creating an expanded word
-                        if (ignore.indexOf(s[i]) < 0) {
-                            anagram[i] = temp[x];
-                            x++;
-                        } else {
-                            anagram[i] = s[i];
-                        }
-                    }
-                    return anagram;
-                }
-
-                public void rev(char s[], int l, int r) {        // reversal array
-                    for (int i = l; i < r / 2; i++) {
-                        char temp = s[i];
-                        s[i] = s[r - 1 - i];
-                        s[r - 1 - i] = temp;
-                    }
-                }
+                outText.setText(edstr.editString(string, ignore));
             }
         };
         button.setOnClickListener(oclButton);
